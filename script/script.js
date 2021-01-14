0e9f67a5c50ed5c12669dc0180f224aeeb7ba1a5
Thales <dapaixaothales@gmail.com>
2021-01-14 18:41:07 -0300

    Atualizações de funções

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
-function verifica(){
+function verifica(operacao){
     a = document.getElementById('catA').value;
     b = document.getElementById('catB').value;
     c = document.getElementById('hip').value;
+    if(Number(a) != a || Number(b) != b || Number(b) != b ){
+        document.getElementById("resposta").innerHTML = "Impossível de calcular. Coloque apenas números.";
+        return false;
+    }
     teste = 0;
     vazio = "";
     if(a == ""){
@@ -16,37 +20,61 @@ function verifica(){
         teste +=1;
         vazio = "c";
     }
-    if(teste != 1){
-        document.getElementById("resposta").innerHTML = "Preencha exatamente dois campos com números.";
-        return false;
-    }else{
-        document.getElementById("resposta").innerHTML = "";
-        return(vazio);
-    }	
+    document.getElementById("resposta").innerHTML = "";
+    document.getElementById("resposta").style.color = "red";
+    if(operacao == 1){
+        if(teste != 1){
+            document.getElementById("resposta").innerHTML = "Preencha exatamente dois campos com números.";
+            return false;
+        }else{
+            return(vazio);
+        }	
+    }
+    if(operacao == 2){
+        if(teste == 0){
+            return true;
+        }else{
+            document.getElementById("resposta").innerHTML = "Preencha todos campos com números.";
+            return false;
+        }	
+    }
 }
 function calcular(){
-    retorno = verifica();
-    //console.log("retorno "+retorno);
+    retorno = verifica(1);
     if(retorno != true){
         a = parseFloat(document.getElementById('catA').value);
         b = parseFloat(document.getElementById('catB').value);
         c = parseFloat(document.getElementById('hip').value);
-        if((Math.abs(b - c) < a && a < (b + c)) && (Math.abs(a - c) < b && b < (a + c)) && (Math.abs(a - b) < c && c < (a + b))){
-            document.getElementById("resposta").innerHTML = "Não forma triângulo.";
-        }else{
-            if(retorno == "a"){
-                a = Math.sqrt((c*c)-(b*b));
-                document.getElementById('catA').value = a;
-            }
-            if(retorno == "b"){
-                b = Math.sqrt((c*c)-(a*a));
-                document.getElementById('catB').value = b;
-            }
-            if(retorno == "c"){
-                c = Math.sqrt((a*a)+(b*b));
-                document.getElementById('hip').value = c;
+        if(retorno == "a"){
+            a = Math.sqrt((c * c)-(b * b));
+            if((b * b) > (c * c)){
+                document.getElementById("catA").value = "sqrt(" + ((c * c) - (b * b)) + ")";
+            }else{
+                document.getElementById('catA').value = a.toPrecision(3);
             }
         }
-        
+        if(retorno == "b"){
+            b = Math.sqrt((c * c) - (a * a));
+            if((a * a) > (c * c)){
+                document.getElementById("catB").value = "sqrt(" + ((c * c) - (a * a)) + ")";
+            }else{
+                document.getElementById('catB').value = b.toPrecision(3);
+            }
+        }
+        if(retorno == "c"){
+            c = Math.sqrt((a * a) + (b * b));
+            document.getElementById('hip').value = c.toPrecision(3);
+        }
     }    
+}
+function ehTriangulo(){
+    verificador = verifica(2)
+    if(verificador == true){
+        document.getElementById("resposta").style.color = "black";
+        if((Math.abs(b - c) < a && a < (b + c)) && (Math.abs(a - c) < b && b < (a + c)) && (Math.abs(a - b) < c && c < (a + b))){
+            document.getElementById("resposta").innerHTML = "Forma um triângulo.";
+        }else{
+            document.getElementById("resposta").innerHTML = "Não forma um triângulo.";
+        }
+    }
 }
